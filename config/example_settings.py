d610068b6fd0da61ebba33e7656ee7ac38918c1e
MarkCutajar <mcut94@gmail.com>
2017-10-29 14:24:57 +0000

    Example and package __init__

---
+++ config/example_settings.py
@@ -0,0 +1,6 @@
+SEED = 28102017
+
+TRAIN_FILES = 'data/data_batch_1, data/data_batch_2, data/data_batch_3, data/data_batch_4'
+VALID_FILES = 'data/data_batch_5'
+TEST_FILES = 'data/test_batch'
+METADATA = 'data/batches.meta'
