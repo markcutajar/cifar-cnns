947753c2e02b8a9286141ae593ae2055d3fb6c49
MarkCutajar <mcut94@gmail.com>
2017-11-05 22:18:56 +0000

    Added attributes for tfrecords

--- config/example_settings.py
+++ config/example_settings.py
@@ -1,6 +1,14 @@
 SEED = 28102017
 
-TRAIN_FILES = 'data/data_batch_1, data/data_batch_2, data/data_batch_3, data/data_batch_4'
-VALID_FILES = 'data/data_batch_5'
-TEST_FILES = 'data/test_batch'
-METADATA = 'data/batches.meta'
+DATA_LOCATION = 'data/'
+
+RAW_TRAIN = 'data/data_batch_1, data/data_batch_2, data/data_batch_3, data/data_batch_4'
+RAW_VALID = 'data/data_batch_5'
+RAW_TEST = 'data/test_batch'
+RAW_META = 'data/batches.meta'
+
+TFR_TRAIN = 'data/cifar10_train'
+TFR_VALID = 'data/cifar10_valid'
+TFR_TEST = 'data/cifar10_test'
+TFR_META = 'data/cifar10_metadata'
+
